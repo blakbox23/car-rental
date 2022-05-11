0f54450c8d4d30315bbf902640d27e0d51f8f66e
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-05-12 00:38:00 +0300

    Set up initial folder structure

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './NavBarStyle.css'; 
+
+function Navbar() {
+  return (
+    <div className='navbar border flex'>
+        <div>Logo</div>
+        <div className='links flex'>
+            <div>Home</div>
+            <div>Rides</div>
+            <div>Services</div>
+            <div>About</div>
+            <div>Reviews</div>
+        </div>
+        <div className='login-section flex'>
+            <div>Login</div>
+            <div>Sign up</div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import NavBar from '../components/NavBar/NavBar'
+
+function Home() {
+  return (
+    <div className='home'>
+        <>
+        <div className='border'>
+            <NavBar />
+        </div>
+     
+        
+        </>
+
+    </div>
+  )
+}
+
+export default Home
