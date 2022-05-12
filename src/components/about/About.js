4f1ac3c09417d15d1179da81516bf80eee890f76
Peter MX <36844547+blakbox23@users.noreply.github.com>
2022-05-12 05:54:57 +0300

    Set up basic about section

---
+++ src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './AboutStyle.css'
+function About() {
+  return (
+    <div className='about'>
+        <div className='about-overlay'>
+        <div className='about-text'>
+        <p className='section-header'>Why rent a car with us? </p>
+        <p className='section-text'>We are open 24/7 including the holidays. Book a ride with us today!</p>
+        <p className='section-text'>It is a long established fact that a reader will be distracted by the readable content
+        'Content here, content here', making it look like readable English. </p>
+        <p className='section-text section-footer'>We are open 24/7 including the holidays. Book a ride with us today!</p>
+
+        
+        </div>
+        </div>
+        
+    </div>
+  )
+}
+
+export default About
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import About from '../components/about/About'
 import Cars from '../components/cars/Cars'
 import Hero from '../components/Hero/Hero'
 import NavBar from '../components/NavBar/NavBar'
@@ -11,6 +12,7 @@ function Home() {
             {/* <NavBar /> */}
             <Hero />
             <Cars />
+            <About />
         </div>
      
         
